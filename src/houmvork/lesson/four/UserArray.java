8558704e932510317c757f6555d9872fad4951f2
Georgvit <ifreyd@mail.ru>
2022-12-03 19:20:38 +0300

    семинар_04

---
+++ src/houmvork/lesson/four/UserArray.java
@@ -0,0 +1,118 @@
+package houmvork.lesson.four;
+
+//Написать приложение для ввода ФИО, возраст и пол пользователей.
+// Данные хранить в разных списках. Сортировать пользователей по возрасту.
+// Не вывести в порядке возрастания возрастов а именно сортировать!)
+
+import java.util.*;
+
+public class UserArray {
+    public static void main(String[] args) {
+        inputArray();
+    }
+
+
+    public static void inputArray() {
+        ArrayList<String> nameUser = new ArrayList<>();
+        ArrayList<String> sexUser = new ArrayList<>();
+        ArrayList<Integer> ageUser = new ArrayList<>();
+
+        //        Заглушка для программы
+//        nameUser.add("Егор Андреевич Сизов");
+//        nameUser.add("Вика Алексеевна Лобанова");
+//        nameUser.add("Лера Егатериновна Петрова");
+//        nameUser.add("Максим Сергеевич Кура");
+//        sexUser.add("муж");
+//        sexUser.add("жен");
+//        sexUser.add("жен");
+//        sexUser.add("муж");
+//        ageUser.add(36);
+//        ageUser.add(20);
+//        ageUser.add(35);
+//        ageUser.add(36);
+
+        int count = 0;
+        System.out.print("Введите количество пользователей: ");
+        Scanner scannerCount = new Scanner(System.in);
+        int finalCount = Integer.parseInt(scannerCount.nextLine());
+        while (count < finalCount) {
+            System.out.println("Введите имя пользователя");
+            Scanner scanner = new Scanner(System.in);
+            nameUser.add(scanner.nextLine());
+            System.out.println("Введите пол пользователя");
+            sexUser.add(scanner.nextLine());
+            System.out.println("Введите возраст пользователя");
+            ageUser.add(scanner.nextInt());
+            count++;
+
+        }
+
+        nameUser.forEach((String s) -> System.out.print(s + " / "));
+        System.out.println();
+        sexUser.forEach((String s) -> System.out.print(s + " / "));
+        System.out.println();
+        ageUser.forEach((Integer i) -> System.out.print(i + " / "));
+        System.out.println();
+        sortData(nameUser, sexUser, ageUser);
+
+    }
+
+    public static void sortData(ArrayList<String> name, ArrayList<String> sex, ArrayList<Integer> age) {
+        System.out.println("""
+                Выберите вариант сортировки:
+                1. По имени\s
+                2. По полу\s
+                3. По возрасту""");
+
+        Scanner scannerChoice = new Scanner(System.in);
+        int choiceUser = scannerChoice.nextInt();
+
+        switch (choiceUser) {
+            case 1:
+                sortDataArray(name, sex, age);
+                break;
+            case 2:
+                sortDataArray(sex, name, age);
+                break;
+            case 3:
+                sortDataArray(age, name, sex);
+                break;
+            default:
+                System.out.println("не корректный ввод");
+        }
+
+
+    }
+
+    public static void sortDataArray(ArrayList One, ArrayList two, ArrayList three) {
+        LinkedList tempArrayOne = new LinkedList<>(One);
+        LinkedList tempArrayTwo = new LinkedList<>();
+        LinkedList tempArrayThree = new LinkedList<>();
+
+        Collections.sort(tempArrayOne);
+        tempArrayOne.forEach(str -> System.out.print(str + " / "));
+        System.out.println();
+
+        for (int i = 0; i < One.size() - 1; i++) {
+            if (One.get(i) != One.get(i + 1)) {
+                for (int j = 0; j < One.size(); j++) {
+                    if (tempArrayOne.get(i) == One.get(j)) {
+                        tempArrayTwo.add(two.get(j));
+                        tempArrayThree.add(three.get(j));
+                    }
+                }
+            }
+        }
+        if (tempArrayTwo.size() < One.size()) {
+            tempArrayTwo.add(two.get(One.size() - 1));
+        }
+        if (tempArrayThree.size() < One.size()) {
+            tempArrayThree.add(three.get(One.size() - 1));
+        }
+        tempArrayTwo.forEach(str -> System.out.print(str + " / "));
+        System.out.println();
+        tempArrayThree.forEach(str -> System.out.print(str + " / "));
+
+
+    }
+}
