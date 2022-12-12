1fa73ff7f65247e040cb1aaa09b22baeccf64937
Georgvit <110120409+Georgvit@users.noreply.github.com>
2022-12-12 14:04:30 +0300

    final commit DZ six

--- src/houmvork/lesson/six/Main.java
+++ src/houmvork/lesson/six/Main.java
@@ -1,16 +1,18 @@
 package houmvork.lesson.six;
 
 import java.util.Random;
+import java.util.TreeSet;
 
 public class Main {
 
-//    Создаем новое множество
-    static  ArraySet arrSet = new ArraySet();
+    // Создаем новое множество
+    static ArraySet arrSet = new ArraySet();
+
     public static void main(String[] args) {
 
         Random random = new Random();
 
-//        Заполняем рандомными значениями и выводим результат
+        // Заполняем рандомными значениями и выводим результат
         for (int i = 0; i < 10; i++) {
             int temp = random.nextInt(100);
             arrSet.add(temp);
@@ -18,7 +20,18 @@ public class Main {
         System.out.println("Множество Set реализуемое через TreeMap");
 
         arrSet.printArr();
+        System.out.println();
 
+        // Создаем новое множество TreeSet
+        TreeSet<Integer> newArrSet = new TreeSet<>(new Sorted());
+        // Заполняем рандомными значениями и выводим результат
+        for (int i = 0; i < 10; i++) {
+            int temp = random.nextInt(100);
+            newArrSet.add(temp);
+        }
+        System.out.println("Множество Set реализуемое через TreeSet");
+
+        newArrSet.forEach(s -> System.out.print(s + " "));
 
     }
 }
